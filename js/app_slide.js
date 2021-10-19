8d05a58c65d11a4abbca1000743942d25fd63427
BeatrizIrineu <bia.irineu14@gmail.com>
2021-10-19 10:50:11 -0300

    css/projeto.css

---
+++ js/app_slide.js
@@ -0,0 +1,12 @@
+const $simpleCarousel = document.querySelector(".js-carousel--simple");
+
+new Glider($simpleCarousel, {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    dots: ".js-carousel--simple-dots",
+    arrows: {
+        prev: ".js-carousel--simple-prev",
+        next: ".js-carousel--simple-next",
+    },
+});
