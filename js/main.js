f5081fcaa67fa16dec0e97a60276d3959b12346d
NeiltonSeguins <seguins.neilton@gmail.com>
2021-10-21 15:54:50 -0300

    Adiciona estilização para dispositivos móveis.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-function navbar() {
+function openMenu() {
+    let navConfig = document.getElementById("navbar");
+    navConfig.style.display = "none";
+    if (navConfig.style.display === "none") {
+        navConfig.style.display = "block";
+    } else {
+        navConfig.style.display = "none";
+    }
+}
+
+function closeMenu() {
     let navConfig = document.getElementById("navbar");
     if (navConfig.style.display === "block") {
         navConfig.style.display = "none";
     } else {
         navConfig.style.display = "block";
     }
-  }
+}
